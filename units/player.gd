e7476cddb446d6bddf3d8c8d8c1f942a0965d2ab
Richard Campen <richard.campen@octoenergy.com>
2024-08-17 11:06:31 +1200

    feat: add basic firing

--- units/player.gd
+++ units/player.gd
@@ -7,14 +7,16 @@ const HORIZONTAL_SPEED = 1000
 const VERTICAL_BUFFER = 50
 const HORIZONTAL_BUFFER = 75
 
+@export var Projectile : PackedScene
+@onready var projectile_spawner = $ProjectileSpawner
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Projectile = load("res://units/projectile.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var viewport_bounds = get_viewport_rect().size
 	
 	if Input.is_action_pressed("up") and global_position.y > 0 + VERTICAL_BUFFER:
@@ -26,3 +28,10 @@ func _process(delta):
 		position.x -= HORIZONTAL_SPEED * delta
 	elif Input.is_action_pressed("right") and global_position.x < viewport_bounds.x - HORIZONTAL_BUFFER:
 		position.x += HORIZONTAL_SPEED * delta
+
+	if Input.is_action_just_pressed("fire"):
+		print('fired!')
+		var p = Projectile.instantiate()
+		owner.add_child(p)
+		p.transform = projectile_spawner.global_transform
+	
