6090e51fa957137a13bd3df81256236f15277e20
Charles Heckroth <checkroth@gmail.com>
2024-08-18 12:02:46 +0900

    Add projectile scaling workaround
    Adds a workaround that manually scales the children of the Projectile
    scene.
    This is due to a problem where the projectile root node scale does not
    propagate down to the sprite or collision shape within it.
    
    https://github.com/godotengine/godot/issues/5734

--- units/projectile.gd
+++ units/projectile.gd
@@ -10,7 +10,8 @@ func spawn(owner: Node, spawn_with_projectile_effects: Array[ProjectileEffect], 
 	projectile_effects = spawn_with_projectile_effects
 	var local_transform = parent_transform
 	for effect in projectile_effects:
-		add_child(effect.instantiate())
+		add_child(effect)
+		effect.set_owner(self)
 		effect.modify_creation(owner, projectile_effects, local_transform)
 	# Spawn the default projectile
 	# TODO:: Projectile spawn modification may want to prevent the default from spawning
@@ -19,6 +20,13 @@ func spawn(owner: Node, spawn_with_projectile_effects: Array[ProjectileEffect], 
 	projectile_effects = projectile_effects
 	transform = local_transform
 
+func scale_projectile(new_scale: Vector2):
+	$Sprite2D.scale = new_scale
+	$CollisionShape2D.scale = new_scale
+
+func _ready():
+	if scale != Vector2.ONE:
+		scale_projectile(scale)
 
 func _physics_process(delta):
 	var position_modifier = transform.x * SPEED * delta
@@ -30,3 +38,6 @@ func _on_area_entered(area):
 	# TODO: apply effects on enemy hit. For now just despawn.
 	queue_free()
 	area.owner.queue_free()
+
+func _integrate_forces(delta):
+	scale = Vector2(100, 100)
