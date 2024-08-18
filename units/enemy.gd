2486958164a8c94b2366b7ad5c3b9820b24ec20f
Richard Campen <richard.campen@octoenergy.com>
2024-08-19 07:16:35 +1200

    feat: add basic logic to remove enemies when hit or offscreen
    - projectiles hitting enemies cause both to be freed
    - enemies offscreen are freed (using notifier node!! thanks @chekroth)

--- units/enemy.gd
+++ units/enemy.gd
@@ -6,3 +6,7 @@ const SPEED = 150
 
 func _physics_process(delta):
 	position -= transform.x * SPEED * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
--- units/projectile.gd
+++ units/projectile.gd
@@ -2,5 +2,12 @@ extends Area2D
 
 const SPEED = 750
 
+
 func _physics_process(delta):
 	position += transform.x * SPEED * delta
+
+
+func _on_area_entered(area):
+	# TODO: apply effects on enemy hit. For now just despawn.
+	queue_free()
+	area.owner.queue_free()
